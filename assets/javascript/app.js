75e6bc1a12fe44f0292c518d162da81b7d60d9c1
J.Lee <joverlee521@gmail.com>
2018-09-24 11:34:25 -0700

    finished simple template of results display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,16 @@
+$("#search-button").on("click", function (){
+    $(".banner").removeClass("page-load");
+    $(".results").removeClass("page-load");
+    $(".page-footer").removeClass("page-load");
+})
+
+
+
+
+
+
 $(document).ready(function(){
     $('.datepicker').datepicker();
     $('select').formSelect();
+    $('.collapsible').collapsible();
   });
