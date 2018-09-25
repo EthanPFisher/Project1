14fd85c3d3f995b106b5e3c403167abfe0f9f661
J.Lee <joverlee521@gmail.com>
2018-09-25 12:17:58 -0700

    changed date-input UI

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,20 @@ $("#search-button").on("click", function (){
     $(".banner").removeClass("page-load");
     $(".results").removeClass("page-load");
     $(".page-footer").removeClass("page-load");
+});
+
+$('#date-icon').on("click", function(event){
+    event.stopPropagation();
+    $('.datepicker').datepicker({"format": "yyyy-mm-dd"});
+    $(".datepicker").datepicker("open");
+});
+
+$("#date-input").on("click", function(){
+    $(".datepicker").datepicker("destroy");
 })
 
 $(document).ready(function(){
-    $('.datepicker').datepicker();
+    $('.datepicker').datepicker({"format": "yyyy-mm-dd"});
     $('select').formSelect();
     $('.modal').modal();
   });
