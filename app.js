985f38aeee3198e4718a2d12721d2af93dfce4f6
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-09-24 23:03:42 -0700

    list of common directionsAPI objects

--- app.js
+++ app.js
@@ -5,4 +5,9 @@
 //-show direction on map using poly lines (animate to show direction)
 //-provide image of what destination looks like as well (street view?)
 
+// URL:
+// https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=YOUR_API_KEY
+
+//Use DirectionsService object to get JSON of directions
+//Use DirectionsRenderer to render rather than doing it ourselves
 
