546c823cdcf908e70a9a2e7b8cd3cc55bd7ac7f5
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-09-24 22:56:58 -0700

    test remote branch commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//GOOGLE DIRECTIONS API
+
+// ---------Best Practices---------??
+//-walking, cycling, driviing directions
+//-show direction on map using poly lines (animate to show direction)
+//-provide image of what destination looks like as well (street view?)
+
+
