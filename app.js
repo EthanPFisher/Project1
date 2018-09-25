d9e3e0459031d239848a0faa8dfd8553be85d985
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-09-25 11:51:25 -0700

    created HTML file to test js. Can return directionsService object between origin and destination

--- app.js
+++ app.js
@@ -11,3 +11,32 @@
 //Use DirectionsService object to get JSON of directions
 //Use DirectionsRenderer to render rather than doing it ourselves
 
+
+var directionsService = new google.maps.DirectionsService();
+
+
+apiKey = "AIzaSyB5lSGcPQkieKi9JEwoRUb2IqZ656nfPl0"
+
+origin = "seattle";
+
+destination = "bellingham";
+
+queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + encodeURIComponent(origin) + "&destination=" + encodeURIComponent(destination) + "&key=" + apiKey
+
+console.log(queryURL);
+
+var directionsRequest = {
+    origin: origin,
+    destination: destination,
+    travelMode: google.maps.DirectionsTravelMode.DRIVING,
+    unitSystem: google.maps.UnitSystem.METRIC
+};
+directionsService.route(directionsRequest, function (response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {                    
+    //do work with response data
+    console.log(response);
+    }
+    else{
+        //Error has occured
+    }
+})
