d0ad13a97cb94b9ad847ad6465daec6038ef2358
J.Lee <joverlee521@gmail.com>
2018-09-25 18:38:06 -0700

    prevent past dates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,8 +15,20 @@ $("#date-input").on("click", function(){
     $(".datepicker").datepicker("destroy");
 })
 
+function preventPastDates(){
+    var todaysDate = new Date(); // Gets today's date
+    // Max date attribute is in "YYYY-MM-DD".  Need to format today's date accordingly
+    var year = todaysDate.getFullYear();                        // YYYY
+    var month = ("0" + (todaysDate.getMonth() + 1)).slice(-2);  // MM
+    var day = ("0" + todaysDate.getDate()).slice(-2);           // DD
+    var minDate = (year +"-"+ month +"-"+ day); // Results in "YYYY-MM-DD" for today's date 
+    // Now to set the max date value for the calendar to be today's date
+    $('#date-input').attr('min',minDate);
+}
+
 $(document).ready(function(){
     $('.datepicker').datepicker({"format": "yyyy-mm-dd"});
+    preventPastDates();
     $('select').formSelect();
     $('.modal').modal();
   });
