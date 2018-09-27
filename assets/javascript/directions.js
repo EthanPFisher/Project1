1ebacc30827a013b057a69de012a0e38afc424cf
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-09-27 12:03:38 -0700

    directions appended in new divs but with HTML tags from GoogleDirections

--- assets/javascript/directions.js
+++ assets/javascript/directions.js
@@ -12,45 +12,49 @@
 //Use DirectionsRenderer to render rather than doing it ourselves
 
 
-var directionsService = new google.maps.DirectionsService();
 
 
-apiKey = "AIzaSyB5lSGcPQkieKi9JEwoRUb2IqZ656nfPl0"
+$(document).ready(function(){
+    var directionsService = new google.maps.DirectionsService();
 
-origin = "seattle";
 
-destination = "bellingham";
-
-queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + encodeURIComponent(origin) + "&destination=" + encodeURIComponent(destination) + "&key=" + apiKey
-
-console.log(queryURL);
-
-var directionsRequest = {
-    origin: origin,
-    destination: destination,
-    travelMode: google.maps.DirectionsTravelMode.DRIVING,
-    unitSystem: google.maps.UnitSystem.IMPERIAL
-};
-directionsService.route(directionsRequest, function (response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {                    
-    //do work with response data
+    apiKey = "AIzaSyB5lSGcPQkieKi9JEwoRUb2IqZ656nfPl0"
     
-    var legs = response.routes[0].legs[0]
-
-        for (var i = 0; i < legs.steps.length; i++ ){
-            console.log(legs.steps[i].instructions + 'for ' + legs.steps[i].distance.text);
-            var directions = legs.steps[i].instructions + 'for ' + legs.steps[i].distance.text
-            var newDiv = $("<p>")
-            newDiv.text(directions);
-            $("#get-directions-button").append(newDiv)
+    origin = "seattle";
+    
+    destination = "bellingham";
+    
+    queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + encodeURIComponent(origin) + "&destination=" + encodeURIComponent(destination) + "&key=" + apiKey
+    
+    console.log(queryURL);
+    
+    var directionsRequest = {
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.DirectionsTravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    };
+    directionsService.route(directionsRequest, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {                    
+        //do work with response data
+        
+        var legs = response.routes[0].legs[0]
+    
+            for (var i = 0; i < legs.steps.length; i++ ){
+                console.log(legs.steps[i].instructions + 'for ' + legs.steps[i].distance.text);
+                var directions = legs.steps[i].instructions + 'for ' + legs.steps[i].distance.text
+                var newDiv = $("<p>")
+                newDiv.text(directions);
+                $("#directions-list").append(newDiv)
+            }
         }
-    }
-    else {
-        //Error has occured
-    }
+        else {
+            //Error has occured
+        }
+    })
+
+    
+    //to get steps of route
+    
+    // response.routes[0].legs[0].steps
 })
-
-
-//to get steps of route
-
-// response.routes[0].legs[0].steps
