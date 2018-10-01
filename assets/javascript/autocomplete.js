bb1099f2e447c13f26d30a420be0e8b7ea94feb4
Simin Shamsfallah <Simin.Shamsfallah@Earlens.com>
2018-09-30 21:54:58 -0700

    Added autocomplete to the start from input in the map

--- assets/javascript/autocomplete.js
+++ assets/javascript/autocomplete.js
@@ -1,9 +1,27 @@
+//Autocomplete the location
 (function() {
     var placesAutocomplete = places({
       container: document.querySelector('#location-input')
     });
   
     var $address = document.querySelector('#location-input')
+    placesAutocomplete.on('change', function(e) {
+      $address.textContent = e.suggestion.value 
+    });
+  
+    placesAutocomplete.on('clear', function() {
+      $address.textContent = 'none';
+    });
+  
+  })();
+  
+  //Autocomplete Start from input in the map 
+  (function() {
+    var placesAutocomplete = places({
+      container: document.querySelector('#from-input')
+    });
+  
+    var $address = document.querySelector('#from-input')
     placesAutocomplete.on('change', function(e) {
       $address.textContent = e.suggestion.value 
     });
