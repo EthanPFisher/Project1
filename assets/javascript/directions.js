19c5a5c74f8119a2118943b897d6781cc072988e
Simin Shamsfallah <Simin.Shamsfallah@Earlens.com>
2018-10-04 14:35:40 -0700

    Added the script for the google map to fix the bug

--- assets/javascript/directions.js
+++ assets/javascript/directions.js
@@ -158,49 +158,6 @@ $(".material-icons").on('click', function () {
     console.log(transportMethod);
 })
 
-var map, infoWindow;
-      function initMap(lat, long) {
-        map = new google.maps.Map(document.getElementById('map-embed'), {
-            zoom: 13,
-            center: { lat: lat, lng: long }
-        });
-        var marker = new google.maps.Marker({
-            position: { lat: lat, lng: long },
-            map: map
-        })
-        directionsDisplay.setMap(map, marker);
-        
-        infoWindow = new google.maps.InfoWindow;
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Current Location.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
-
 
 $(".nav-icons").on('click',function(){
     console.log('test')
